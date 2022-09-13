99b4348e3e83afeaeab5919f816b1cd5e1bc8cf3
meetiboon7 <meet.iboon@gmail.com>
2022-09-13 18:42:28 +0530

    AllPatientReport done

--- application/controllers/admin/AllPatientReport.php
+++ application/controllers/admin/AllPatientReport.php
@@ -44,9 +44,6 @@ class AllPatientReport extends GeneralController {
 	public function generateAllPatientReport(){
 
 		$post = $this->input->post(NULL, true);
-		
-		// $start_date=$post['from_date'];
-		// $end_date=$post['to_date'];
 
 		$start_date      = date("Y-m-d",strtotime($post['from_date']));
         $end_date        = date("Y-m-d",strtotime($post['to_date']));
@@ -55,40 +52,32 @@ class AllPatientReport extends GeneralController {
 		$this->excel->getActiveSheet()->setTitle('All Patient Report');
 		$this->excel->getActiveSheet()->setCellValue('A1', 'Date');
 		$this->excel->getActiveSheet()->setCellValue('B1', 'Patient');
-		$this->excel->getActiveSheet()->setCellValue('C1', 'Service');
-		$this->excel->getActiveSheet()->setCellValue('D1', 'Total');
-		$this->excel->getActiveSheet()->setCellValue('E1', 'Discount');
-		$this->excel->getActiveSheet()->setCellValue('F1', 'Voucher');
-		$this->excel->getActiveSheet()->setCellValue('G1', 'Credit');
+		$this->excel->getActiveSheet()->setCellValue('C1', 'Contact No');
+		$this->excel->getActiveSheet()->setCellValue('D1', 'Service Taken');
+		$this->excel->getActiveSheet()->setCellValue('E1', 'Address');
+		$this->excel->getActiveSheet()->setCellValue('F1', 'Service done/cancel');
+		$this->excel->getActiveSheet()->setCellValue('G1', 'Receipt');
+		$this->excel->getActiveSheet()->setCellValue('H1', 'City');
+		$this->excel->getActiveSheet()->setCellValue('I1', 'Service provider name');
 
-
-        // $this->db->select('appointment_book.txndate AS [d_date], book_nurse.txndate AS n_date, book_laboratory_test.txndate AS l_date, book_ambulance.txndate AS a_date, member.name, appointment_book.service AS d_service, nurse_service_type.nurse_service_name, lab_test_type.lab_test_type_name, book_ambulance.service AS a_service, appointment_book.total AS d_total, book_laboratory_test.total AS l_total, book_nurse.total AS n_total, book_ambulance.amount, appointment_book.discount, book_nurse.discount AS n_discount, appointment_book.voucher, book_nurse.voucher AS n_voucher, appointment_book.credit, book_nurse.credit AS n_credit');
-
-        $this->db->select('appointment_book.txndate , book_nurse.txndate AS n_date, member.name, appointment_book.service AS d_service, nurse_service_type.nurse_service_name, appointment_book.total AS d_total, book_nurse.total AS n_total, appointment_book.discount, book_nurse.discount AS n_discount, appointment_book.voucher, book_nurse.voucher AS n_voucher, appointment_book.credit, book_nurse.credit AS n_credit');
-        $this->db->from('member');
-        $this->db->join('appointment_book', 'appointment_book.patient_id = member.member_id', 'LEFT');
-        $this->db->join('book_nurse', 'book_nurse.patient_id = member.member_id', 'LEFT');
-        $this->db->join('nurse_service_type','nurse_service_type.nurse_service_id = book_nurse.type','LEFT');
-        // $this->db->join('book_laboratory_test', 'book_laboratory_test.patient_id = member.member_id', 'LEFT');
-        // $this->db->join('lab_test_type', 'lab_test_type.lab_test_id = book_laboratory_test.lab_test_id', 'LEFT');
-        // $this->db->join('book_ambulance','book_ambulance.patient_id = member.member_id','LEFT');
-        $this->db->where('txndate BETWEEN "'.$start_date. '" and "'. $end_date.'"');
-        $this->db->where('n_date BETWEEN "'.$start_date. '" and "'. $end_date.'"');
-        // $this->db->where('DATE(l_date) BETWEEN "'.$start_date. '" and "'. $end_date.'"');
-        // $this->db->where('DATE(a_date) BETWEEN "'.$start_date. '" and "'. $end_date.'"');
-        $setSql = $this->db->get()->result_array();
-        echo $this->db->last_query();
-		exit;
+		$query = $this->db->query("SELECT extra_invoice.date, member.name, member.contact_no, extra_invoice.list, member.address, IF(extra_invoice.type != '' , 'DONE', 'CANCEL'), extra_invoice.price, city.city, IF(extra_invoice.appointment_book_id != 0 , 'Doctor', IF(extra_invoice.book_nurse_id != 0 , 'Nurse', IF(extra_invoice.book_laboratory_test_id != 0 , 'Laboratory', IF(extra_invoice.book_ambulance_id != 0 , 'Ambulance', NULL)))) AS service_name
+		FROM extra_invoice
+		LEFT JOIN member ON member.member_id = extra_invoice.patient_id
+		LEFT JOIN user ON user.user_id = member.member_id 
+		LEFT JOIN appointment_book ON appointment_book.appointment_book_id = extra_invoice.appointment_book_id
+		LEFT JOIN book_nurse ON book_nurse.book_nurse_id = extra_invoice.book_nurse_id
+		LEFT JOIN nurse_service_type ON nurse_service_type.nurse_service_id = book_nurse.type
+		LEFT JOIN book_laboratory_test ON book_laboratory_test.book_laboratory_test_id = extra_invoice.book_laboratory_test_id
+		LEFT JOIN lab_test_type ON lab_test_type.lab_test_id = book_laboratory_test.lab_test_id
+		LEFT JOIN book_ambulance ON book_ambulance.book_ambulance_id = extra_invoice.book_ambulance_id
+		LEFT JOIN city ON city.city_id = member.city_id
+		WHERE DATE( extra_invoice.date ) BETWEEN  '$start_date' AND  '$end_date'");
+        $setSql = $query->result_array();
         $exceldata = array();
 		if(count($setSql)>0)
 		{
 	       foreach ($setSql as $row){
 	            $exceldata[] = $row;
-	        }
-			foreach ($exceldata as $key => $value) 
-	        {
-	            $value['service'] = !empty($value['service']) ? '' : 'Doctor Type';
-	            $exceldata[$key] = $value;
 	        }
 		} 
 		else
@@ -106,7 +95,8 @@ class AllPatientReport extends GeneralController {
         $this->excel->getActiveSheet()->getStyle('G3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
         $this->excel->getActiveSheet()->getStyle('H3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
         $this->excel->getActiveSheet()->getStyle('I3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-        $filename='Tele_Consulting'; //save our workbook as this file name
+		
+        $filename='All_Patient'; //save our workbook as this file name
         header('Content-Type: application/vnd.ms-excel'); //mime type
         // header('Content-Disposition: attachment;filename="'.$filename.'"'); 
         header("Content-Disposition: attachment; filename=".$filename."_Reoprt.xls");
